d5b50406bb66f76d754891c8cb7a999f4264f228
Akash Sawan <akash27274@gmail.com>
2024-04-08 08:11:55 +0530

    adding to github

---
+++ setup.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE akashsawan;
+create user 'akash'@'localhost' identified by 'akash';
+USE akashsawan;
+
+CREATE TABLE test (myfield VARCHAR(20));
+
+INSERT INTO test VALUES ('Hello'), ('Dolly');
